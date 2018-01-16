9fa1c7e3be8acf826e72c836e6aef77d51e17754
golfiarz <piotrekgrzywna@gmail.com>
2018-01-16 16:44:19 +0100

    test calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(5,4);
+
+        System.out.println(resultAdd);
+
+        int resultSub = calculator.substract(5, 4);
+
+        System.out.println(resultSub);
+
+
     }
 }
