35e39825d0ccbb30964199c38fc0303840e60cb1
golfiarz <piotrekgrzywna@gmail.com>
2018-01-23 17:21:45 +0100

    poprawiona wersja

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,14 +1,10 @@
 package com.kodilla.testing.collection;
+
 import java.util.*;
 import java.lang.*;
 
 public class OddNumbersExterminator {
-    ArrayList<Integer> even;
 
-
-    public OddNumbersExterminator() {
-        even = new ArrayList<Integer>();
-    }
 
     public ArrayList<Integer> exterminate(ArrayList<Integer> number) {
 
@@ -16,21 +12,17 @@ public class OddNumbersExterminator {
         int counter = number.size();
 
 
-            for (int n = 0; n < counter; n++) {
-                int numberValue = number.get(n);
-                if (numberValue % 2 == 0) {
-                    even.add(numberValue);
+        for (int n = 0; n < counter; n++) {
+            int numberValue = number.get(n);
+            if (numberValue % 2 == 0) {
+                even.add(numberValue);
 
-                }
             }
-        for (Integer numberNewList : even) {
-                System.out.println(numberNewList);
-                }
-
+        }
 
         return even;
 
     }
-    }
+}
 
 
