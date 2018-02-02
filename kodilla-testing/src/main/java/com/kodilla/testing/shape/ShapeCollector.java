5d48b62e8b5ba3ff5344fe6ed4318ce3f4b21cde
golfiarz <piotrekgrzywna@gmail.com>
2018-02-02 20:28:32 +0100

    task6.4cdn

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -3,15 +3,40 @@ package com.kodilla.testing.shape;
 import java.util.ArrayList;
 
 public class ShapeCollector {
-    private Shape shape;
-    private ArrayList<Shape> shapes = new ArrayList<>();
-
-    public void ShapeCollector(Shape shape) {
-        this.shape = shape;
-    }
+ArrayList<Shape> shapes = new ArrayList<>();
 
     public void addFigure(Shape shape) {
-       this.shape.toString() ;
+       Shape shape = new Shape() {
+           @Override
+           public String getShapeName() {
+               return null;
+           }
+
+           @Override
+           public int getField() {
+               return 0;
+           }
+       }
        shapes.add(shape);
+    }
+
+    public boolean removeFigure(Shape shape) {
+        boolean result = false;
+        if (shapes.contains(shape)) {
+            shapes.remove(shape);
+            result = true;
+        }
+    }
+
+    public ArrayList<Shape> getFigure(int n){
+        ArrayList<Shape> shapes = null;
+        if (n >= 0 && n < shapes.size()) {
+            shape = shapes.get(n);
+        }
+        return shape;
+    }
+
+    public ArrayList<Shape> getShapes() {
+        return shapes;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuits.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuits.java
@@ -1,17 +1,24 @@
 package com.kodilla.testing.shape;
 
+import org.junit.Assert;
 import org.junit.Test;
+import com.kodilla.testing.shape.ShapeCollector;
+import com.kodilla.testing.shape.Shape;
+import com.kodilla.testing.shape.Square;
+import com.kodilla.testing.shape.Triangle;
+import com.kodilla.testing.shape.Circle;
 
 public class ShapeCollectorTestSuits {
 
     @Test
     public void testProcess(){
+        //Given
         Shape circle = new Circle("Circle", 5);
-
-        ShapeCollector shapeCollector = new ShapeCollector(circle);
-
-        shapeCollector.addFigure();
-
+        ShapeCollector shapeCollector = new ShapeCollector();
+        //When
+        shapeCollector.addFigure(circle);
+        //Then
+        Assert.assertEquals(1,shapeCollector.getShapes);
 
     }
 }
