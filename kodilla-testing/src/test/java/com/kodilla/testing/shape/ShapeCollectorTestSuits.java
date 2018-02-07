69ec027e496b0b33e6f5a1e2c405a4e5bc0a27f8
golfiarz <piotrekgrzywna@gmail.com>
2018-02-07 15:58:52 +0100

    task6.4

--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuits.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuits.java
@@ -11,14 +11,38 @@ import com.kodilla.testing.shape.Circle;
 public class ShapeCollectorTestSuits {
 
     @Test
-    public void testProcess(){
+    public void testAddFigure(){
         //Given
         Shape circle = new Circle("Circle", 5);
         ShapeCollector shapeCollector = new ShapeCollector();
         //When
         shapeCollector.addFigure(circle);
         //Then
-        Assert.assertEquals(1,shapeCollector.getShapes);
+        Assert.assertEquals(1, shapeCollector.getShapes().size());
 
+    }
+
+    @Test
+    public void testRemoveFigure(){
+        //Given
+        Shape circle = new Circle("Circle", 5);
+        ShapeCollector shapeCollector = new ShapeCollector();
+        shapeCollector.addFigure(circle);
+        //When
+        shapeCollector.removeFigure(circle);
+        //Then
+        Assert.assertEquals(0,shapeCollector.getShapes().size());
+    }
+
+    @Test
+    public void testGetFigure() {
+        //Given
+        Shape circle = new Circle("Circle", 5);
+        ShapeCollector shapeCollector = new ShapeCollector();
+        shapeCollector.addFigure(circle);
+        //When
+        shapeCollector.getFigure(0);
+        //Then
+        Assert.assertEquals(shapeCollector.getShapes().get(0), shapeCollector.getFigure(0));
     }
 }
