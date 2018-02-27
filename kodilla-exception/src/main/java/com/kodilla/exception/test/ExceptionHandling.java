e3b09fc7e11277c73851e900f62dc32d932aae0c
golfiarz <piotrekgrzywna@gmail.com>
2018-02-27 11:32:52 +0100

    task8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,21 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args){
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(2,1);
+        } catch (MyException e) {
+            System.out.println("Problem " + e.getMessage());
+
+        } finally {
+
+            System.out.println("Checked numbers");
+
+        }
+
+    }
+}
