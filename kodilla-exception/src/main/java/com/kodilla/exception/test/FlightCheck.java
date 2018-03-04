8f6df3cf803997c168cfaf2432f60550b8cbb5bf
golfiarz <piotrekgrzywna@gmail.com>
2018-03-04 13:16:23 +0100

    task8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightCheck.java
@@ -0,0 +1,29 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class FlightCheck {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        HashMap<String, Boolean> airports = new HashMap<>();
+        airports.put("Jasionka", true);
+        airports.put("Stansed", true);
+
+            if (airports.get(flight.getArrivalAirport()) == true) {
+
+                System.out.println("Available");
+            } else {
+
+                System.out.println("Not");
+            }
+
+
+            throw new RouteNotFoundException();
+
+
+        }
+    }
+
+
+
