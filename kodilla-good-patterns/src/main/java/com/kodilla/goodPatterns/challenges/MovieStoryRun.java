ba3c0082b05e72803304b68dec2205553309497f
golfiarz <piotrekgrzywna@gmail.com>
2018-03-08 16:31:24 +0100

    task9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/goodPatterns/challenges/MovieStoryRun.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/goodPatterns/challenges/MovieStoryRun.java
@@ -1,11 +1,24 @@
 package com.kodilla.goodPatterns.challenges;
 import com.kodilla.goodPatterns.challenges.MovieStory;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class MovieStoryRun {
     public static void main(String[] args){
-        MovieStory.getMovies().stream()
+        MovieStory movieStory = new MovieStory();
+
+        String movieStoryList = movieStory.getMovies().entrySet().stream()
+                .map(k -> k.getValue())
+                .map(l ->l.toString())
+                .collect(Collectors.joining("!","<<", ">>"));
+
+        System.out.println(movieStory);
 
 
 
     }
 }
+
