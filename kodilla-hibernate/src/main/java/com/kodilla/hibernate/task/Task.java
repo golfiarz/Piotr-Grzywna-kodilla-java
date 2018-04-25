30fde47107cb9fdd9f5e1dee1565aa366365cd07
golfiarz <piotrekgrzywna@gmail.com>
2018-04-25 12:47:36 +0200

    task13.2(Enity)

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -10,7 +10,7 @@ import java.util.Date;
 
 @Entity
 @Table(name = "tasks")
-public class Task {
+public final class Task {
     private int id;
     private String description;
     private Date created;
